8f16bbb0e760a804425da288625e894c06f4da01
Egor Kisel <kisel1989@mail.ru>
2022-03-14 18:07:10 +0300

    Исправил EditNoteActivity класс

--- app/src/main/java/com/geekbrains/notes/ui/EditNoteActivity.java
+++ app/src/main/java/com/geekbrains/notes/ui/EditNoteActivity.java
@@ -4,17 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.geekbrains.notes.R;
+import com.geekbrains.notes.data.InMemoryRepoImp;
 import com.geekbrains.notes.data.Note;
+import com.geekbrains.notes.data.Repo;
 
-public class EditNoteActivity extends AppCompatActivity implements View.OnClickListener {
+public class EditNoteActivity extends AppCompatActivity{
 
+    private int id;
     private Note note;
     Button save;
+    EditText title;
+    EditText description;
+    private Repo repo = InMemoryRepoImp.getInstance();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,7 +31,14 @@ public class EditNoteActivity extends AppCompatActivity implements View.OnClickL
         // TODO создать внешний вид для редактирования заметки
 
         setContentView(R.layout.activity_edit_note);
-        findViewById(R.id.button_save).setOnClickListener(this);
+        title = findViewById(R.id.edit_note_title);
+        description = findViewById(R.id.edit_note_description);
+        title.setText(note.getTitle());
+        description.setText(note.getDescription());
+        save = findViewById(R.id.button_save);
+        save.setOnClickListener(v ->{
+            saveNote();
+        });
 
     }
 
@@ -32,15 +46,18 @@ public class EditNoteActivity extends AppCompatActivity implements View.OnClickL
     // TODO написать возвращение отредактированной заметки в NotesListActivity
 
 
-    void saveNote() {
+    public void saveNote() {
+        int noteId = note.getId();
+        Note editNote = note;
+
+        editNote.setTitle(title.getText().toString());
+        editNote.setDescription(description.getText().toString());
+        editNote.setId(noteId);
+        repo.update(editNote);
         Intent result = new Intent();
-        result.putExtra(Note.NOTE, note);
+        result.putExtra(Note.NOTE, this.note);
         setResult(RESULT_OK, result);
         finish();
     }
 
-    @Override
-    public void onClick(View view) {
-        saveNote();
-    }
 }
--- app/src/main/java/com/geekbrains/notes/ui/NotesListActivity.java
+++ app/src/main/java/com/geekbrains/notes/ui/NotesListActivity.java
@@ -9,11 +9,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.geekbrains.notes.R;
 import com.geekbrains.notes.data.InMemoryRepoImp;
 import com.geekbrains.notes.data.Note;
 import com.geekbrains.notes.data.Repo;
+import com.geekbrains.notes.recycler.NoteHolder;
 import com.geekbrains.notes.recycler.NotesAdapter;
 
 public class NotesListActivity extends AppCompatActivity implements NotesAdapter.onNoteClickListener {
@@ -21,6 +24,8 @@ public class NotesListActivity extends AppCompatActivity implements NotesAdapter
     private RecyclerView list;
     private Repo repo = InMemoryRepoImp.getInstance();
     private NotesAdapter adapter;
+    private TextView title;
+    private TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,8 @@ public class NotesListActivity extends AppCompatActivity implements NotesAdapter
         list.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         list.setAdapter(adapter);
         list.setLayoutManager(new LinearLayoutManager(this));
+        title = findViewById(R.id.note_title);
+        description = findViewById(R.id.note_description);
     }
 
     public static final int EDIT_NOTE_REQUEST = 66;
@@ -40,7 +47,6 @@ public class NotesListActivity extends AppCompatActivity implements NotesAdapter
     @Override
     public void onNoteClick(Note note) {
         Log.d("happy", note.getDescription());
-
         Intent editNoteIntent = new Intent(this, EditNoteActivity.class);
         editNoteIntent.putExtra(Note.NOTE, note);
         startActivityForResult(editNoteIntent, EDIT_NOTE_REQUEST);
@@ -51,10 +57,8 @@ public class NotesListActivity extends AppCompatActivity implements NotesAdapter
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // Intent intent = getIntent();
-        if(resultCode==RESULT_OK)
-        {
-            assert data != null;
+        if(requestCode==EDIT_NOTE_REQUEST && resultCode==RESULT_OK) {
+            if (data == null) throw new AssertionError();
             Note note = (Note) data.getSerializableExtra(Note.NOTE);
             repo.update(note);
             adapter.setNotes(repo.getAll());
